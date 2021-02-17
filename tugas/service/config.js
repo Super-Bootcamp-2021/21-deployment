45496e47a21b4651998a0c75cad158d429bc19d4
dimas1997 <dimas6597@gmail.com>
2021-02-17 13:44:43 +0700

    start tugas

---
+++ tugas/service/config.js
@@ -0,0 +1,30 @@
+const rc = require('rc');
+
+const defaultConfig = {
+  pg_database: {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '',
+    database: 'database',
+  },
+  minio_database: {
+    endPoint: '192.168.0.8',
+    port: 9000,
+    useSSL: false,
+    accessKey: 'minioadmin',
+    secretKey: 'minioadmin',
+  },
+  server: {
+    taskPort: 7002,
+    workerPort: 7001,
+    performancePort: 7003,
+  },
+};
+
+const config = rc('tm', defaultConfig);
+
+module.exports = {
+  config,
+};
